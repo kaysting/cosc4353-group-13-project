92f68af5313e09a4c6e685caed2dd627220f8756
Kayla Kersting <kayla@kaysting.dev>
2025-07-14 21:15:28 +0000

    Scaffold server API endpoints

--- server.js
+++ server.js
@@ -4,9 +4,44 @@ const app = express();
 // Use Express' built-in JSON parser
 app.use(express.json());
 
+// Create new account
+app.post('/api/auth/register', (req, res) => { });
 
-// API routes will go here...
+// Log into account with username/email and password, returns a session token
+app.post('/api/auth/login', (req, res) => { });
 
+// Log out and delete the current session token
+app.post('/api/auth/logout', (req, res) => { });
+
+// Get current user profile info
+app.get('/api/profile', (req, res) => { });
+
+// Update current user profile info
+app.post('/api/profile/update', (req, res) => { });
+
+// Get events assigned to the current user
+app.post('/api/profile/events', (req, res) => { });
+
+// Get all events (admin only)
+app.get('/api/events', (req, res) => { });
+
+// Create new event (admin only)
+app.post('/api/events/create', (req, res) => { });
+
+// Update existing event info (admin only)
+app.post('/api/events/update', (req, res) => { });
+
+// Get volunteers that are available for a certain event (admin only)
+app.get('/api/events/match/check', (req, res) => { });
+
+// Assign a volunteer to an event (admin only)
+app.post('/api/events/match/assign', (req, res) => { });
+
+// Get notifications for the current user
+app.get('/api/notifications', (req, res) => { });
+
+// Get volunteer history (maybe admin only?)
+app.get('/api/history', (req, res) => { });
 
 // Serve static files from the public directory
 app.use(express.static('public'));
